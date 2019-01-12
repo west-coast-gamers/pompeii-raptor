5c4271bcb64d95bdad3a651fca5418be5d6afd27
Fritjof Aderstedt Althoff <fritjof@Eriks-rMBP.local>
2019-01-12 14:52:56 +0100

    Guld

---
+++ Guldmatare.gd
@@ -0,0 +1,13 @@
+extends Label
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var balance = 0
+var smycken = {"silverpeng": 1, "guldpeng": 2, "rubin": 3, "smaragd": 4, "safir": 5, "diamant": 6}
+
+func _laggtill(smycke):
+	var varde = smycken[smycke]
+	balance += varde
+	self.set_text("Guld: " +  str(balance))
+
