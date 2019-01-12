a14a7a60dfe72abd0592618c224c389659e7841d
Wranker Johan <johan.wranker@volvo.com>
2019-01-12 14:43:40 +0000

    added basic swag

--- map.gd
+++ map.gd
@@ -16,6 +16,7 @@ func load(filename, game_area):
 	loaded_map['grass_positions'] = []
 	loaded_map['wall_positions'] = []
 	loaded_map['tree_positions'] = []
+	loaded_map['swag_positions'] = []
 
 	for y in range(game_area.height_in_tiles):
 		if file.eof_reached():
@@ -31,6 +32,8 @@ func load(filename, game_area):
 				loaded_map.wall_positions.append(Vector2(x, y))
 			elif c == 'T':
 				loaded_map.tree_positions.append(Vector2(x, y))
+			elif c == 'S':
+				loaded_map.swag_positions.append(Vector2(x, y))
 			else:
 				oprint ("wrong tile type")
 
