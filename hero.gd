526c68e15107d0b0ee92d6904962fed83cf6696e
Wranker Johan <johan.wranker@volvo.com>
2019-01-13 21:26:08 +0000

    Adding running hero

--- hero.gd
+++ hero.gd
@@ -11,12 +11,16 @@ func _ready():
 
 func _process(delta) :
 	var velocity = Vector2(0, 0)
+	var direction = 0
 	if Input.is_action_pressed('hero_right'):
 		velocity.x += 1
+		direction += 0
 	if Input.is_action_pressed('hero_left'):
 		velocity.x -= 1
+		direction += 180
 	if Input.is_action_pressed('hero_up'):
 		velocity.y -= 1
+		direction += 90
 	if Input.is_action_pressed('hero_down'):
 		velocity.y += 1
 	if Input.is_action_just_pressed('hero_do'):
@@ -24,8 +28,15 @@ func _process(delta) :
 
 	velocity = velocity.normalized() * Hero_Speed
 	hero_velocity = velocity
-
+	
 	move_and_collide(hero_velocity * delta)
+	
+	var speed = sqrt(velocity.x*velocity.x + velocity.y*velocity.y)
+	if speed>0:
+		$"hero-01".rotation = atan2(hero_velocity.y,hero_velocity.x)
+		if $"hero-01/anim".current_animation != "running":
+			$"hero-01/anim".play("running")
+		
 
 var _item = null
 func isCloseToMe(item):
