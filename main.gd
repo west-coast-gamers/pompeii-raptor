8b8476dba8db41d7342feeaf523861bac634dba3
Erik Aderstedt <erik@aderstedtsoftware.com>
2019-01-13 18:09:36 +0100

    Test version of city wall

--- main.gd
+++ main.gd
@@ -19,6 +19,7 @@ var tree_scene = load('res://tree.tscn')
 var swag_scene = load('res://swag.tscn')
 var decorations_scene = load('res://decorations.tscn')
 var city_wall_scene = load('res://city_wall.tscn')
+var city_wall_gate_scene = load('res://city_wall_gate.tscn')
 
 # :Tips - dictionaries works like a struct in some sense since it can be
 # access using a . syntax, e.g. game_area.offset.
@@ -93,6 +94,8 @@ func _ready():
 
 	else:
 		print('Failed to load map')
+		
+	create_city_wall_polygons()
 
 func _process(delta):
 	if Input.is_action_pressed('debug_f1'):
@@ -102,41 +105,58 @@ func _on_world_ap_animation_finished(anim_name):
 	# @Incomplete - inactivate world?
 	$"houses/house-01".enter_house()
 
-func get_city_wall_polygons():
-	randomize()
-	
-	var wall_segment = city_wall.instance()
-	
-	var p0 = Vector2(1000,1000)
-	var vertices = [p0]
-	var p1 = p0
-	
-	var number_of_corners = rand_range(1,6) + 6
-	var angle_per_segment = 2.0*PI / number_of_corners
-	var standard_segment_length = 100.0
-	var angle_variation = PI/5
-	var current_angle = PI/2 + rand_range(0, angle_variation) - 0.5*angle_variation
 
-	print(p0)
-	for i in range(1,number_of_corners):
-		var length = standard_segment_length + rand_range(1,int(standard_segment_length))
-		var p2 = p1 + Vector2(cos(current_angle)*length, sin(current_angle)*length)
-		if p0.distance_to(p2) > standard_segment_length:
-			vertices.append(p2)
-			var t = rand_range(0, angle_variation) - 0.5*angle_variation
+const THICKNESS = 25.0
+const STANDARD_SEGMENT_LENGTH = 300.0
+const ANGLE_VARIATION = PI/7
+
+func add_wall_segment(start_of_segment, end_of_segment, angle):
+	var wall_segment = city_wall_scene.instance()	
+	var p2d = wall_segment.get_node("Border")
+	var diff = Vector2(cos(angle + PI/2)*THICKNESS*0.5, sin(angle + PI/2)*THICKNESS*0.5)
+	var vertices = [start_of_segment + diff, end_of_segment + diff, end_of_segment - diff, start_of_segment - diff]
+	p2d.polygon = PoolVector2Array(vertices)
+	var p2df = wall_segment.get_node("BorderFill")
+	p2df.polygon = PoolVector2Array(vertices)
+	p2df.texture_rotation = angle + PI/2
+	$world.add_child(wall_segment)
+				
+func create_city_wall_polygons():
+	randomize()
+
+	var start_of_wall = Vector2(250,550)
+	var start_of_segment = start_of_wall
+	var number_of_corners = randi()%6 + 6
+	var segments_with_gates = [0, int(number_of_corners/4), int(number_of_corners/2), int(3*number_of_corners/4)]
+	var current_angle = PI/2 + rand_range(-0.5*ANGLE_VARIATION, 0.5*ANGLE_VARIATION)
+	var start_angle = current_angle
+
+	for i in range(0,number_of_corners):
+		var angle_per_segment = (2*PI - (start_angle - current_angle))/(number_of_corners - i)
 		
+		var length = STANDARD_SEGMENT_LENGTH + rand_range(-0.3*STANDARD_SEGMENT_LENGTH, 0.3*STANDARD_SEGMENT_LENGTH)
+		var end_of_segment = start_of_segment + Vector2(cos(current_angle)*length, sin(current_angle)*length)
+			
+		var next_angle
+		if i == number_of_corners - 1:
+			end_of_segment = start_of_wall
+			current_angle = start_of_segment.angle_to_point(end_of_segment)
+			next_angle = 0
+		else:
+			var t = rand_range(-0.5*ANGLE_VARIATION, 0.5*ANGLE_VARIATION)
+			next_angle = current_angle - angle_per_segment - t			
+			var tower_angle = current_angle + PI - (PI - (current_angle - next_angle))/2
 		
-			current_angle -= angle_per_segment + t;
-			print(p2.x, ",",p2.y)
-			p1 = p2
+		add_wall_segment(start_of_segment, end_of_segment, current_angle)					
+		if segments_with_gates.find(i-1) != -1:				
+			var gate = city_wall_gate_scene.instance()
+			gate.position = start_of_segment + 0.5*(end_of_segment - start_of_segment)
+			gate.rotation = current_angle
+			
+			$world.add_child(gate)
+			
+
+			
+		current_angle = next_angle
+		start_of_segment = end_of_segment
 	
-	vertices.append(p0)
-	print(p0)
-	
-	# Marina port is on the first segment
-	# North gate, east gate, south gate
-	
-	# Determine segments for the four gates.
-	#
-	# If there is a gate on this segment, we add two polygons.
-	# 
